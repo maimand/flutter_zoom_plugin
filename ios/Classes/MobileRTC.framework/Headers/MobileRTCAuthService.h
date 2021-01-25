83eb153d7cc44eab3b667e5bb61b48e76bcdf82f
= <=>
2021-01-25 14:54:15 +0700

    change the sdk

--- ios/Classes/MobileRTC.framework/Headers/MobileRTCAuthService.h
+++ ios/Classes/MobileRTC.framework/Headers/MobileRTCAuthService.h
@@ -2,7 +2,7 @@
 //  MobileRTCAuthService.h
 //  MobileRTC
 //
-//  Created by Robust Hu on 8/8/14.
+//  Created by Zoom Video Communications on 8/8/14.
 //  Copyright (c) 2019 Zoom Video Communications, Inc. All rights reserved.
 //
 
@@ -79,6 +79,13 @@
  @warning The method is optional, ignore it if you do not login MobileRTC with SSO.
  */
 - (BOOL)loginWithSSOToken:(nonnull NSString*)token rememberMe:(BOOL)rememberMe;
+
+/*!
+ @brief Query if it is enabled to login with email.
+ @return YES means enable, otherwise not.
+ @warning You need call the function after auth successfull.
+ */
+- (BOOL)isEmailLoginEnabled;
 
 /*!
  @brief Specify to logout MobileRTC.
@@ -190,7 +197,7 @@
  @brief Get alternative host list from user profile information.
  @return An array with MobileRTCAlternativeHost information.
  */
-- (nullable NSArray*)getCanScheduleForUsersList;
+- (nullable NSArray <MobileRTCAlternativeHost *> *)getCanScheduleForUsersList;
 
 /*!
  @brief Check if local recording is supported while scheduling a meeting.
@@ -220,7 +227,7 @@
  @brief Get specified domain from user profile.
  @return The data in domain array is NSString type.
  */
-- (nullable NSArray *)getDefaultCanJoinUserSpecifiedDomains;
+- (nullable NSArray <NSString *> *)getDefaultCanJoinUserSpecifiedDomains;
 
 @end
 
